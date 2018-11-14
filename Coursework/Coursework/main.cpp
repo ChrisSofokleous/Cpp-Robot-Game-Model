c64ed95aad09cd51db07210b522f96ff1947bd74
ChrisSofokleous <christophersofokleous@gmail.com>
2018-11-14 17:04:14 +0000

    Initial Commit

---
+++ Coursework/Coursework/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Coursework
+//
+//  Created by Christopher Sofokleous on 14/11/2018.
+//  Copyright © 2018 Christopher Sofokleous. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
